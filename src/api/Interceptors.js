934c9ce8ec6bc92c6c6c9b5864e5d6a9029ff6a6
IvarOt <150660814+IvarOt@users.noreply.github.com>
2024-12-10 19:04:46 +0100

    fixed logout after invalid session

--- src/api/Interceptors.js
+++ src/api/Interceptors.js
@@ -1,33 +1,31 @@
 import { instance } from "../api/api"
 import { getLocalAccesToken, UpdateLocalAccesToken } from "../services/TokenService";
-import { useAuth } from "../contexts/AuthContext";
 
-instance.interceptors.response.use(
-    (response) => {
-        return response;
-    },
-    async function (error) {
-        const originalRequest = error.config;
-        if (error.response.status === 401 && !originalRequest.retry) {
-            originalRequest.retry = true;
-            console.log("Refreshing access token...");
-            try {
-                const response = await instance.post("/player/refresh-token");
-                console.log(response);
-                const access_token = response.data;
-                UpdateLocalAccesToken(access_token);
-                originalRequest.headers.Authorization = 'Bearer ' + access_token;
-                return instance(originalRequest);
+export const setupAxiosInterceptors = (logout) => {
+    instance.interceptors.response.use(
+        (response) => response,
+        async (error) => {
+            const originalRequest = error.config;
+            if (error.response.status === 401 && !originalRequest.retry) {
+                originalRequest.retry = true;
+                console.log("Refreshing access token...");
+                try {
+                    const response = await instance.post("/player/refresh-token");
+                    console.log(response);
+                    const access_token = response.data;
+                    UpdateLocalAccesToken(access_token);
+                    originalRequest.headers.Authorization = 'Bearer ' + access_token;
+                    return instance(originalRequest);
+                } catch (error) {
+                    logout();
+                    return Promise.reject(error);
+                }
             }
-            catch (error) {
-                useAuth().logout();
-                return Promise.reject(error);
-            }
+            logout();
+            return Promise.reject(error);
         }
-        useAuth().logout();
-        return Promise.reject(error);
-    }
-  );
+    );
+};
 
 instance.interceptors.request.use(
     (config) => {
