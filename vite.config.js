505d75b2fc4c009601e983a5632a42d408e025b2
Ivar <Ivarottenheym@gmail.com>
2024-12-23 17:47:21 +0100

    configured to use environment variables

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const apiUrl = process.env.VITE_API_URL || "https://localhost:7146/"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, 
+    port: 4173,
+  },
+  define: {
+    API_URL: JSON.stringify(apiUrl),
+  },
 })
